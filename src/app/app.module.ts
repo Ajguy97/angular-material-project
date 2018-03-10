5da8fe67e712a6aa947d952615e3c6734b0a2831
Andre Godinez <ajguy97@gmail.com>
2018-03-10 19:57:47 +0000

    Started version control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
+import {MaterialModule} from './material.module';
 
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+  /**
+   *  Import all the modules the main Module needs to bootstrap
+   */
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    /**  @Important
+     - Angular material needs hammerjs -> import to apps entry point -> src/main.ts
+     - Angular material has prebuilt themes -> need import them to styles.css - not component.css
+     */
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,12 @@
+/**
+ *  This class is for all the Angular Material components that we use
+ *  We need to import them and also export
+ */
+
+import { NgModule} from '@angular/core';
+
+@NgModule({
+  imports: [],
+  exports: []
+})
+export class MaterialModule{}
